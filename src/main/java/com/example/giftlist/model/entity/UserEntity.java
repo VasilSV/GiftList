5176c0fe737daa8c54bfa9c8cb567a79a03ed5f0
VasilSV <114056331+VasilSV@users.noreply.github.com>
2024-07-21 19:41:38 +0300

    start

--- src/main/java/com/example/giftlist/model/entity/UserEntity.java
+++ src/main/java/com/example/giftlist/model/entity/UserEntity.java
@@ -1,7 +1,10 @@
 package com.example.giftlist.model.entity;
 
 
+import com.example.giftlist.model.enums.UserTypeEntity;
 import jakarta.persistence.*;
+
+import javax.validation.constraints.Email;
 
 @Entity
 @Table(name = "users")
@@ -11,5 +14,15 @@ public class UserEntity {
 @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(nullable = false)
-    private String username;
+    private String firstName;
+    @Column(nullable = false)
+    private String lastName;
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserTypeEntity userTypeEntity;
+
+
+
 }

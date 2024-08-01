71095b50a0c6f7908b5871c4fa3d31d7bd06e0d2
VasilSV <114056331+VasilSV@users.noreply.github.com>
2024-08-01 13:45:53 +0300

    new

--- src/main/java/com/example/giftlist/model/entity/UserEntity.java
+++ src/main/java/com/example/giftlist/model/entity/UserEntity.java
@@ -29,5 +29,57 @@ public class UserEntity {
     public UserEntity() {
     }
 
+    public Long getId() {
+        return id;
+    }
 
+    public UserEntity setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public UserEntity setFirstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public UserEntity setLastName(String lastName) {
+        this.lastName = lastName;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public UserEntity setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public UserEntity setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public UserTypeEntity getUserTypeEntity() {
+        return userTypeEntity;
+    }
+
+    public UserEntity setUserTypeEntity(UserTypeEntity userTypeEntity) {
+        this.userTypeEntity = userTypeEntity;
+        return this;
+    }
 }
